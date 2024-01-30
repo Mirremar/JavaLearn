2a9f91f63bd9504b31617da8fa661204dfc8beeb
Main Villain <deserteagle081@gmail.com>
2024-01-30 21:58:43 +0300

    files created

---
+++ Lesson1_task2.java
@@ -0,0 +1,12 @@
+package Java;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Lesson1_task2 
+{
+    //FileReader fr = new FileReader("L1task2testfile1.txt");
+}
